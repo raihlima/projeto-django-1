305c4806b48b3919f6b758cf5328b1d704a74fc0
Raih Lima <raih.cesar@ice.ufjf.br>
2022-05-02 12:21:00 -0300

    Utilizando Faker

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from util.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe for _ in range(15)],
+    })
 
 
 def receitas(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
