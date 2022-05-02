900683117c6015c625c8cd4538f7ba3630f1a658
Raih Lima <raih.cesar@ice.ufjf.br>
2022-05-02 12:36:41 -0300

    Nome unico das urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('receitas/<int:id>/', views.receitas),
+    path('', views.home, name="recipes-home"),
+    path('receitas/<int:id>/', views.receitas, name="recipes-recipe"),
 ]
