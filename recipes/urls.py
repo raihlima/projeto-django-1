0e3ebc464112d05f394e8f62f3b16cc0aa71288d
Raih Lima <raih.cesar@ice.ufjf.br>
2022-05-04 12:30:06 -0300

    Models do banco de dados

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home, name="recipes-home"),
-    path('receitas/<int:id>/', views.receitas, name="recipes-recipe"),
+    path('', views.home, name="home"),
+    path('receitas/<int:id>/', views.receitas, name="recipe"),
 ]
